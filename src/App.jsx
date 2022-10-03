8521d2db53f7d12abfc836eb8cd0490580554b05
kienphan91 <phanngockien010591@gmail.com>
2022-10-03 09:28:58 +0700

    update-debug

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import Banner from "./components/banner/Banner"
-import Navbar from "./components/Navbar"
+import Banner from "./components/banner/Banner";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div className="h-1/2 lg:h-screen bg-gradient-to-t from-indigo-200 font-nunito relative overflow-hidden">
-      <Navbar/>
-      <Banner/>
+    <div className="aspect-[2/1] lg:aspect-[4/3]	 bg-gradient-to-t from-indigo-200 font-nunito relative overflow-hidden">
+      <Navbar />
+      <Banner />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
